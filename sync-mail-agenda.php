a822545f41c55a77fc2f6da77c39c255ff53bfdc
florian HENRY <florian.henry@atm-consulting.fr>
2016-07-13 16:58:04 +0200

    log debug

--- sync-mail-agenda.php
+++ sync-mail-agenda.php
@@ -46,8 +46,9 @@ function _sync_mailbox($usertodo, $host, $mailbox, $login, $password, $labelForS
 	// $mbox = imap_open($host, $login, $password);
 	$mbox = imap_open($host . $mailbox, $login, $password);
 
+	var_dump($mbox);
 	if ($mbox === false) {
-		print "Connexion impossible  $host, $mailbox, $login";
+		print "Connexion impossible  $host, $mailbox, $login<br />";
 		return false;
 	} else {
 		print "Connect ok $host, $mailbox, $login<br />";
